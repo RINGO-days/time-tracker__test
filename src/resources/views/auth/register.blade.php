0ec66de2b2ebf8dca81bd0a43b1f64ea5850fadb
RINGO_days <Apple043@outlook.jp>
2026-05-23 21:04:20 +0900

    会員登録画面作成

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>会員登録</title>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/register.css')}}">
+@endsection
+
+@section('main')
+    <h1 class="page-title">会員登録</h1>
+    <form action="" method="post" novalidate>
+    @csrf
+        <div class="form-box">
+            <div class="input-box">
+                <label class="input-title">名前
+                    <input class="input" type="text" name="name">
+                </label>
+            </div>
+            <div class="input-box">
+                <label class="input-title">メールアドレス
+                    <input class="input" type="email" name="email">
+                </label>
+            </div>
+            <div class="input-box">
+                <label class="input-title">パスワード
+                    <input class="input" type="password" name="password">
+                </label>
+            </div>
+            <div class="input-box">
+                <label class="input-title">パスワード確認
+                    <input class="input" type="password" name="password">
+                </label>
+            </div>
+            <button class="button__submit" type="sumbit">登録する</button>
+        </div>
+    </form>
+    <div class="login-link__inner">
+        <a href="" class="login-link">ログインはこちら</a>
+    </div>
+@endsection
