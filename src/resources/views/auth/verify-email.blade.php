a14f442ead2bf0ef212ab2100a3984d315e8855a
RINGO_days <Apple043@outlook.jp>
2026-05-24 14:14:11 +0900

    ログイン画面機能作成 close #6,close #7,close #8,close #9,close #10

---
+++ src/resources/views/auth/verify-email.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>メール認証</title>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/verify.css')}}">
+@endsection
+
+@section('main')
+    <p class="email-description">登録していただいたメールアドレスに認証メールを送付いたしました。<br>メール認証を完了してください。</p>
+        <div class="verify-item__inner">
+            <a class="mail-link" href="http://localhost:8025">認証はこちらから</a>
+            <form action="">
+                @csrf
+                <button class="resend-button" type="submit">認証メールを再送信する</button>
+            </form>
+        </div>
+
+@endsection
