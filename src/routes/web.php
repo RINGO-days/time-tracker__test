584b24bb863ad6e2be4cd4bc20d35f0c4103440b
RINGO_days <Apple043@outlook.jp>
2026-05-24 20:50:22 +0900

    管理者用ログイン画面作成 close #13,close #14,close #15,close #16

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\AdminController;
 
 route::get('/attendance',[AttendanceController::class,'Attendance']);
+route::get('/show',[AdminController::class,'show']);
+
+
 
 
