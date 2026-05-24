fa44f064e08bf4fbef106cb7fd8acb957a5a64d1
RINGO_days <Apple043@outlook.jp>
2026-05-24 21:43:14 +0900

    管理者ページログアウト機能実装 close #17

---
+++ src/resources/views/admin/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>勤怠一覧</title>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/auth.css')}}">
+@endsection
+
+@section('main')
+@endsection
