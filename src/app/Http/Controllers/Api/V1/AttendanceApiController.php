0da34fe8b48c85cae971e7a2fed57c87bd59c7c0
RINGO_days <Apple043@outlook.jp>
2026-06-28 12:16:27 +0900

    indexメソッド作成

---
+++ src/app/Http/Controllers/Api/V1/AttendanceApiController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Attendance;
+
+class AttendanceApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->query('per_page',20);
+        if($perPage > 100){
+            $perPage = 100;
+        }
+
+        $query = Attendance::query();
+        if($request->query('user_id')){
+            $query->where('user_id',$request->query('user_id'));
+        }
+
+        if($request->query('date')){
+            $query->whereDate('attendance_date',$request->query('date'));
+        }
+
+        if($request->query('month')){
+            $query->whereMonth('attendance_date',$request->query('month'));
+        }
+        $attendances = $query->paginate($perPage);
+        if($attendances->isEmpty()){
+            return response()->json([
+                'message' => '出勤記録がありませんでした。',
+                'error' => 'RECORDS_NOT_FOUND'
+            ],404);
+        }
+        return response()->json([
+            'data' => $attendances
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request,$id)
+    {
+        $query = Attendance::query();
+
+        if($request->query('user_id')){
+            $query->where('user_id',$request->query('user_id'));
+        }
+        if($request->query('date')){
+            $query->whereDate('attendance_date',$request->query('date'));
+        }
+        if($request->query('month')){
+            $query->whereMonth('attendance_date',$request->query('month'));
+        }
+
+        $attendance = $query->find($id);
+
+        if(!$attendance){
+            return response()->json([
+                'message' => '出勤記録がありませんでした。',
+                'error' => 'ATTENDANCE_NOT_FOUND'
+            ],404);
+        }
+        return response()->json([
+            'data' => $attendance
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
