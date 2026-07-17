a6330a1a6764155f3955c1023e30e6d1c8be8678
RINGO_days <Apple043@outlook.jp>
2026-07-17 11:33:15 +0900

    フラッシュメッセージの変更

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -74,7 +74,7 @@ class FortifyServiceProvider extends ServiceProvider
                     return back()->with('message','一般スタッフ用のページにはログインできません。');
                 }elseif(!auth()->user()->is_admin && str_contains(url()->previous(),'admin/login')){
                     auth()->logout();
-                    return back()->with('message','管理者ページにはログインの権限がありません。');
+                    return back()->with('message','管理者ページにログインの権限がありません。');
                 }
 
                 return redirect('/');
